8e57767ba464c8aec4a2a689b3f9c5b5d1f615dc
Toby Searle <14909402+twsearle@users.noreply.github.com>
2023-08-03 09:38:11 +0100

    fix the plots to plot only the caps

--- plotSphericalCapsOnSphere.py
+++ plotSphericalCapsOnSphere.py
@@ -1,44 +1,140 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 from optimalSphericalCapHeight import optimize_cap_height
 
 def spherical_cap_coordinates(R, num_caps):
-    cap_height = optimize_cap_height(R, num_caps)
-    cap_theta = np.linspace(0, 2 * np.pi, num_caps, endpoint=False)
-    cap_phi = np.arccos(cap_height / R)
+    if num_caps not in [4, 6, 8, 12]:
+        raise ValueError("Number of caps must be 4, 6, 8, or 12.")
 
-    cap_centers_x = R * np.sin(cap_phi) * np.cos(cap_theta)
-    cap_centers_y = R * np.sin(cap_phi) * np.sin(cap_theta)
-    cap_centers_z = R * np.cos(cap_phi)
+    cap_centers = []
 
-    cap_centers = np.column_stack((cap_centers_x, cap_centers_y, cap_centers_z))
+    if num_caps == 4:  # Tetrahedron
+        vertices = np.array([
+            [1.0, 1.0, 1.0],
+            [-1.0, -1.0, 1.0],
+            [-1.0, 1.0, -1.0],
+            [1.0, -1.0, -1.0]
+        ])
+    elif num_caps == 6:  # Octahedron
+        vertices = np.array([
+            [0.0, 1.0, 0.0],
+            [0.0, -1.0, 0.0],
+            [1.0, 0.0, 0.0],
+            [-1.0, 0.0, 0.0],
+            [0.0, 0.0, 1.0],
+            [0.0, 0.0, -1.0]
+        ])
+    elif num_caps == 8:  # Cube
+        vertices = np.array([
+            [1.0, 1.0, 1.0],
+            [-1.0, -1.0, 1.0],
+            [-1.0, 1.0, -1.0],
+            [1.0, -1.0, -1.0],
+            [1.0, 1.0, -1.0],
+            [-1.0, -1.0, -1.0],
+            [-1.0, 1.0, 1.0],
+            [1.0, -1.0, 1.0]
+        ])
+    elif num_caps == 12:  # Icosahedron
+        phi = (1 + np.sqrt(5)) / 2  # Golden ratio
 
-    return cap_centers
+        vertices = np.array([
+            [0, 1, phi],
+            [0, -1, phi],
+            [0, 1, -phi],
+            [0, -1, -phi],
+            [1, phi, 0],
+            [-1, phi, 0],
+            [1, -phi, 0],
+            [-1, -phi, 0],
+            [phi, 0, 1],
+            [-phi, 0, 1],
+            [phi, 0, -1],
+            [-phi, 0, -1]
+        ])
 
-def plot_spherical_caps_on_sphere(R, num_caps):
+    for vertex in vertices:
+        vertex = vertex / np.linalg.norm(vertex)
+        cap_centers.append(R * vertex)
+
+    return np.array(cap_centers)
+
+def plot_spherical_caps(R, num_caps):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
-    phi = np.linspace(0, np.pi, 100)
-    theta = np.linspace(0, 2 * np.pi, 100)
-    phi, theta = np.meshgrid(phi, theta)
-
-    x = R * np.sin(phi) * np.cos(theta)
-    y = R * np.sin(phi) * np.sin(theta)
-    z = R * np.cos(phi)
-
     cap_centers = spherical_cap_coordinates(R, num_caps)
+    cap_height = optimize_cap_height(R, num_caps)
 
     for center in cap_centers:
-        ax.plot_surface(x + center[0], y + center[1], z + center[2], color='b', alpha=0.4)
+        phi = np.linspace(0, np.pi / 2, 30)
+        theta = np.linspace(0, 2 * np.pi, 30)
+        phi, theta = np.meshgrid(phi, theta)
+
+        x = center[0] + R * np.sin(phi) * np.cos(theta)
+        y = center[1] + R * np.sin(phi) * np.sin(theta)
+        z = center[2] + R * np.cos(phi)
+
+        ax.plot_surface(x, y, z, color='b', alpha=0.4)
 
     ax.set_xlim([-R, R])
     ax.set_ylim([-R, R])
     ax.set_zlim([-R, R])
     ax.set_aspect('auto')
     plt.show()
+
+def rotation_matrix_from_vectors(vec1, vec2):
+    """Calculate the rotation matrix that rotates vec1 to vec2."""
+    # Ensure that the vectors are unit vectors (normalized)
+    vec1 = vec1 / np.linalg.norm(vec1)
+    vec2 = vec2 / np.linalg.norm(vec2)
+
+    # Calculate the cross product and the dot product
+    v = np.cross(vec1, vec2)
+    c = np.dot(vec1, vec2)
+
+    # Calculate the skew-symmetric cross product matrix
+    skew_sym_matrix = np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
+
+    # Calculate the rotation matrix
+    R = np.identity(3) + skew_sym_matrix + np.dot(skew_sym_matrix, skew_sym_matrix) * (1 / (1 + c))
+
+    return R
+
+def plot_spherical_caps_on_sphere(R, num_caps, vector=np.array([0, 0, 1])):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    cap_centers = spherical_cap_coordinates(R, num_caps)
+    cap_height = optimize_cap_height(R, num_caps)
+
+    for center in cap_centers:
+        phi = np.linspace(0, np.pi / 2, 30)
+        theta = np.linspace(0, 2 * np.pi, 30)
+        phi, theta = np.meshgrid(phi, theta)
+
+        x = R * np.sin(phi) * np.cos(theta)
+        y = R * np.sin(phi) * np.sin(theta)
+        z = R * np.cos(phi)
+
+        # Calculate the rotation matrix to align the cap with the vector
+        cap_normal = center / np.linalg.norm(center)
+        rotation_matrix = rotation_matrix_from_vectors(cap_normal, vector)
+
+        # Rotate the cap coordinates
+        x_rot, y_rot, z_rot = np.einsum('ij,jkl->ikl', rotation_matrix, np.array([x, y, z]))
+
+        ax.plot_surface(x_rot, y_rot, z_rot, color='b', alpha=0.4)
+
+    ax.set_xlim([-R, R])
+    ax.set_ylim([-R, R])
+    ax.set_zlim([-R, R])
+    ax.set_aspect('auto')
+    plt.show()
+
 
 def main():
     R = 1.0  # Change the radius of the sphere here
